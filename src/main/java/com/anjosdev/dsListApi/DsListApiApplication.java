94dda6779f7f739d13baf253964eb5851fd520f4
Vitória Anjos <109820733+anjosvitoria@users.noreply.github.com>
2025-01-24 17:46:07 -0300

    Initial commit

---
+++ src/main/java/com/anjosdev/dsListApi/DsListApiApplication.java
@@ -0,0 +1,13 @@
+package com.anjosdev.dsListApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsListApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsListApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anjosdev/dsListApi/DsListApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anjosdev.dsListApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsListApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
